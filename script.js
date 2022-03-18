01a59ecc63925de0eb2f80e01b1be28934315e0a
Glitch (glitch-hello-website) <none>
2022-03-18 07:38:00 +0000

    🎥👲 Checkpoint ./index.html:41144138/66 ./script.js:41144138/802

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-let count
-count = 0
+let count = 0
+let proton = 0
+let neutron = 0
+let electron = 0
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function increment()
   {
-    count = count + 1
-    if (count == 1){
-      document.getElementById("counter").innerHTML = "Atom: " + count
+    if (count < 50){
+      count += 1
+      if (count == 1){
+        document.getElementById("counter").innerHTML = "Atom: " + count
+      }
+      else{
+        document.getElementById("counter").innerHTML = "Atoms: " + count
+      }
     }
     else{
-      document.getElementById("counter").innerHTML = "Atoms: " + count
+      if (getRandomInt(3) == 0){
+        proton += 1
+      }
+      if (getRandomInt(3) == 1){
+        neutron += 1
+      }
+      if (getRandomInt(3) == 2){
+        electron += 1
+      }
+      
+      document.getElementById("counter").innerHTML = 
+        "Proton: " + proton +
+        "\n" + "Neutron: " + neutron +
+        "\n" + "Electron: " + electron
+      
     }
   }
