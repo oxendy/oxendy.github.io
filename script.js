6f260d39076f6853650b021251187d7c95532240
Glitch (glitch-hello-website) <none>
2022-03-18 07:26:01 +0000

    🔨🏟 Checkpoint ./script.js:41144138/44 ./index.html:41144138/25

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ count = 0
 function increment()
   {
     count = count + 1
-    if count == 1:
+    if (count == 1){
       document.getElementById("counter").innerHTML = "Atom: " + count
-    else:
+    }
+    else{
       document.getElementById("counter").innerHTML = "Atoms: " + count
+    }
   }
