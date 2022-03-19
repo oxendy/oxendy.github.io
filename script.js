c8793c8a2356363faa5ef80aafec1b9970eb69ab
Glitch (glitch-hello-website) <none>
2022-03-19 10:20:51 +0000

    📉🎆 Checkpoint ./script.js:41144138/2002

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+window.addEventListener('load', function()
+{
+    var xhr = null;
+
+    getXmlHttpRequestObject = function()
+    {
+        if(!xhr)
+        {               
+            // Create a new XMLHttpRequest object 
+            xhr = new XMLHttpRequest();
+        }
+        return xhr;
+    };
+
+    updateLiveData = function()
+    {
+        var now = new Date();
+        // Date string is appended as a query with live data 
+        // for not to use the cached version 
+        var url = 'livefeed.txt?' + now.getTime();
+        xhr = getXmlHttpRequestObject();
+        xhr.onreadystatechange = evenHandler;
+        // asynchronous requests
+        xhr.open("GET", url, true);
+        // Send the request over the network
+        xhr.send(null);
+    };
+
+    updateLiveData();
+
+    function evenHandler()
+    {
+        // Check response is ready or not
+        if(xhr.readyState == 4 && xhr.status == 200)
+        {
+            dataDiv = document.getElementById('liveData');
+            // Set current data text
+            dataDiv.innerHTML = xhr.responseText;
+            // Update the live data every 1 sec
+            setTimeout(updateLiveData(), 1000);
+        }
+    }
+});
+
+
+
+
 //Scrolling title
 (function titleScroller(text) {
     document.title = text;
@@ -36,8 +83,16 @@ function updateAtom(){
 }
 
 function compoundAtom(atom){
-  let particle = compoundList.atom
-  console.log(particle)
+  let particle = compoundList[atom]
+  if ( proton >= particle[0] && neutron >= particle[1] && electron >= particle[2] ){
+    proton -= particle[0]
+    neutron -= particle [1]
+    electron -= particle [2]
+    let temp = atoms.get(atom)
+    temp += 1
+    atoms.set(atom, temp)
+    updateAtom()
+  }
   
 }
 
@@ -56,7 +111,7 @@ function getRandomInt(max) {
 
 function increment()
   {
-    if (count < 50){
+    if (count < 20){
       count += 1
       if (count == 1){
         document.getElementById("counter").innerHTML = "Atom: " + count
